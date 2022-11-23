7a3441e00c58fae0f438cad57c02655c0efdde6f
Eduardo Landeros <elanderos318@gmail.com>
2022-11-22 18:35:58 -0600

    add controller routes and tests

--- src/points/points.controller.spec.ts
+++ src/points/points.controller.spec.ts
@@ -1,22 +1,89 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PointsController } from './points.controller';
-import { TransactionService } from './transaction.service';
+
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { PointsModule } from './points.module';
+import { Transaction } from './interfaces/transaction.interface';
+import { SpendPointsDto } from './dto/spend-points.dto';
+import MockDate from 'mockdate';
 
 describe('PointsController', () => {
-  let pointsController: PointsController;
+  let response: request.Response;
+  let app: INestApplication;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [PointsController],
-      providers: [TransactionService],
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [PointsModule],
     }).compile();
-
-    pointsController = app.get<PointsController>(PointsController);
+    app = module.createNestApplication();
+    await app.init();
   });
+  afterAll(async () => await app.close());
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(pointsController.getHello()).toBe('hello');
+  describe('POST /points/transaction', () => {
+    const timestamp = new Date('2022-10-31');
+    let createTransactionDto: CreateTransactionDto;
+
+    beforeAll(async () => {
+      createTransactionDto = {
+        payer: 'DANNON',
+        points: 500,
+        timestamp,
+      };
+      response = await request(app.getHttpServer())
+        .post('/points/transaction')
+        .send(createTransactionDto);
+    });
+
+    it('should respond with 201', () => {
+      expect(response.statusCode).toBe(201);
+    });
+    it('should return expected transaction', () => {
+      expect(response.body).toEqual(
+        toClientFormat(createTransactionDto) as Transaction,
+      );
     });
   });
+
+  describe('POST /points/spend', () => {
+    const mockDate = new Date('2022-11-01');
+    let spendPointsDto: SpendPointsDto;
+
+    beforeAll(async () => {
+      MockDate.set(mockDate);
+      spendPointsDto = {
+        points: 250,
+      };
+      response = await request(app.getHttpServer())
+        .post('/points/spend')
+        .send(spendPointsDto);
+    });
+
+    it('should respond with 201', () => {
+      expect(response.statusCode).toBe(201);
+    });
+    it('should return expected transactions', () => {
+      expect(response.body).toEqual(
+        toClientFormat([
+          { payer: 'DANNON', points: -250, timestamp: mockDate },
+        ]) as Transaction[],
+      );
+    });
+  });
+
+  describe('GET /points/balances', () => {
+    beforeAll(async () => {
+      response = await request(app.getHttpServer()).get('/points/balances');
+    });
+
+    it('should respond with 200', () => {
+      expect(response.statusCode).toBe(200);
+    });
+    it('should return expected transaction', () => {
+      expect(response.body).toEqual({ DANNON: 250 });
+    });
+  });
+
+  const toClientFormat = (object: any) => JSON.parse(JSON.stringify(object));
 });
--- src/points/points.controller.ts
+++ src/points/points.controller.ts
@@ -1,7 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { SpendPointsDto } from './dto/spend-points.dto';
+import { PayerBalances } from './interfaces/payer-balances.interface';
+import { Transaction } from './interfaces/transaction.interface';
 import { TransactionService } from './transaction.service';
 
 @Controller('points')
 export class PointsController {
   constructor(private readonly transactionService: TransactionService) {}
+
+  @Post('transaction')
+  @ApiOperation({ description: 'POST payer transacaction success ' })
+  @HttpCode(201)
+  @UsePipes(new ValidationPipe({ transform: true }))
+  createTransaction(
+    @Body() createTransactionDto: CreateTransactionDto,
+  ): Transaction {
+    return this.transactionService.createTransaction(createTransactionDto);
+  }
+
+  @Post('spend')
+  @ApiOperation({ description: 'POST spend points success ' })
+  @HttpCode(201)
+  @UsePipes(new ValidationPipe({ transform: true }))
+  spendPoints(@Body() spendPointsDto: SpendPointsDto): Transaction[] {
+    return this.transactionService.spendPoints(spendPointsDto);
+  }
+
+  @Get('balances')
+  @ApiOperation({ description: 'GET payer balances success ' })
+  @HttpCode(200)
+  getBalances(): PayerBalances {
+    return this.transactionService.getPayerBalances();
+  }
 }
